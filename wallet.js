afa587cd6d0b7de7d0b9f97374f5148ed91668e6
Tomasz Sygut <thomassygut@gmail.com>
2019-10-15 19:30:32 +0200

    xyz

---
+++ wallet.js
@@ -0,0 +1,33 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+        // pobieranie aktualnej zawarttosci portfela
+        this.getWalletValue = () => _money;
+        // Sprawdzanie czy uzytkownik ma odpowiednią ilosc srodkow.
+        this.checkCanPLay = value => {
+            if (money >= value) return true;
+            return false;
+        }
+        this.changeWallet = (value, type ="+") => {
+            if (typeof value === "number" && !isNaN(value))
+            {
+                if(type === "+") {
+                    return _money += value;
+                }
+                else if (type === "-") {
+                    return _money -= value;
+                }
+                else
+                {
+                    throw new Error ("Nieprawidłowy typ działania")
+                }
+            
+            }
+            else
+            {
+                console.log(typeof value);
+                throw new Error ("Nieprawidłowa liczba")
+            }
+        }
+    }
+}
