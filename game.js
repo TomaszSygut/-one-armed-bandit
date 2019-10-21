77dd14ae486aedc2fc86a89249718b1e24a130a9
Tomasz Sygut <thomassygut@gmail.com>
2019-10-21 22:20:17 +0200

    GAME WORKS

--- game.js
+++ game.js
@@ -1,43 +1,65 @@
 class Game {
     constructor(start) {
-        this.stats = new Statistic()
-        this.wallet = new Wallet(start)
-        document.getElementById('start').addEventListener('click', this.startGame)
-        this.spanWallet = document.querySelector('.panel span.wallet');
-        this.squareColors = document.querySelectorAll('div.color');
-        this.inputBid = document.getElementById('bid');
-        this.spanResult = document.querySelector('.score span.result');
-        this.spanGames = document.querySelector('.score span.number');
-        this.spanWins = document.querySelector('.score span.win');
-        this.spanLosses = document.querySelector('.score span.loss');
-        
-
-        this.render()
+   
+     this.stats = new Statistics();
+     this.wallet = new Wallet(start);
+   
+     document.getElementById('start').addEventListener('click', this.startGame.bind(this));
+     this.spanWallet = document.querySelector('.panel span.wallet');
+     this.boards = [...document.querySelectorAll('div.color')];
+     this.inputBid = document.getElementById('bid');
+     this.spanResult = document.querySelector('.score span.result');
+     this.spanGames = document.querySelector('.score span.number');
+     this.spanWins = document.querySelector('.score span.win');
+     this.spanLosses = document.querySelector('.score span.loss');
+   
+     this.render()
+   
     }
-
-    render(colors = ['#2ee', '#2ee', '#2ee' ] , money = this.wallet.getWalletValue(), result = "", stats = [0,0,0], bid, wonMoney) {
-
-        this.squareColors.forEach((square,i) => {
-            square.style.backgroundColor = colors[i]
-
-        })
-        if(result){
-            result = `Wygrałeś ${wonMoney}`;
-        }else if (!result && result != "") {
-            result = `Przegrałeś ${bid}`;
-        }
-        
-        this.spanResult.textContent = result;
-        this.spanWallet.textContent = money;
-        this.spanGames.textContent = stats[0];
-        this.spanWins.textContent = stats [0];
-        this.spanLosses.textContent = stats [0];
-
+   
+    render(colors = ['#2ee', '#2ee', '#2ee'], money = this.wallet.getWalletValue(), result = "", stats = [0, 0, 0], bid = 0, wonMoney = 0) {
+     // console.log("gramy!!");
+   
+     this.boards.forEach((board, i) => {
+      board.style.backgroundColor = colors[i]
+     })
+   
+     this.spanWallet.textContent = money;
+     if (result) {
+      result = `YOU WON ${wonMoney} $. `;
+     } else if (!result && result !== "") {
+      result = `YOU LOSS ${bid} $. `
+     }
+     this.spanResult.textContent = result;
+     this.spanGames.textContent = stats[0];
+     this.spanWins.textContent = stats[1];
+     this.spanLosses.textContent = stats[2];
+   
+     this.inputBid.value = "";
     }
-
+   
     startGame() {
-
+        if (this.inputBid.value == "") return alert('Enter some amount');
+     else if (this.inputBid.value < 1) return alert('Your bid is too small!!')
+     
+     const bid = Math.floor(this.inputBid.value);
+   
+     if (!this.wallet.checkCanPlay(bid)) {
+      return alert("You don't have enough funds on your bank account (You can deposit money into your account)")
+     }
+   
+     this.wallet.changeWallet(bid, '-');
+   
+     this.draw = new Draw();
+     const colors = this.draw.getDrawResult();
+     const win = Result.checkWinner(colors);
+     const wonMoney = Result.moneyWinInGame(win, bid);
+     this.wallet.changeWallet(wonMoney);
+     this.stats.addGameToStatistics(win, bid);
+   
+     this.render(colors, this.wallet.getWalletValue(), win, this.stats.showGameStatistics(), bid, wonMoney)
+   
     }
-}
-
-const game = new Game(100);
+   }
+   
+   
--- statistic.js
+++ statistic.js
@@ -1,21 +1,26 @@
-class Statistic {
-    constructor(){
-        this.gameResults = [{win: true, bid: 2}, {win: false, bid: -12}];
-
+class Statistics {
+    constructor() {
+     this.gameResults = [];
+   
     }
-    addGameToStatistics(win,bid) {
-        let gameResult = {
-             win,
-             bid
-        }
-        console.log(gameResult);
-        this.gameResults.push(gameResult)
+   
+    addGameToStatistics(win, bid) {
+     let gameResult = {
+      win,
+      bid
+     }
+     // console.log(gameResult);
+     this.gameResults.push(gameResult)
     }
-    showGameStatistic() {
-        let games = this.gameResults.length
-        let wins = this.gameResults.filter(result => result.win).length
-        let losses = this.gameResults.filter(result = !result.win).length
-        return [games,wins,losses];
+   
+    showGameStatistics() {
+     let games = this.gameResults.length;
+     let wins = this.gameResults.filter(result => result.win).length;
+     let losses = this.gameResults.filter(result => !result.win).length
+     // console.log(games, wins, losses);
+     return [games, wins, losses]
     }
-}
-// const stats = new Statistics()
+   
+   }
+   
+   // const stats = new Statistics()
